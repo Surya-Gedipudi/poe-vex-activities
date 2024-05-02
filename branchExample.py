b7bb4dd5b897c6df13d6c155f52c874bca838065
Surya Gedipudi <surya.gedipudi@gmail.com>
2024-05-02 13:57:03 -0400

    Create branchExample.py

---
+++ branchExample.py
@@ -0,0 +1,12 @@
+"""
+Name:           branchExample.py
+Coded by:       Surya Gedipudi, Ritwik Singth
+Purpose:        Demo to use Gi
+Section:        POE-4
+"""
+
+counter = 0;
+
+while (counter < 10):
+    print("Time: " + str(counter))
+    counter += 1
