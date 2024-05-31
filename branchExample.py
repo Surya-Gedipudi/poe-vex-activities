fb36e431c474c40f9e208045ea75b83f97994ecf
Ritwik Singh <singhritwik710@gmail.com>
2024-05-31 09:10:12 -0400

    asdas

---
+++ branchExample.py
@@ -0,0 +1,11 @@
+"""
+Name: branchExample.py
+Coded By: R. Singh & S. Gedipudi
+Purpose Demo how to use Github branches & commits
+Date 5/1/24
+"""
+
+counter = 0
+while (counter< 10):
+    print("Time: ", counter)
+    counter += 1
